14dfa588749a6554e85018950c3bbf413658b799
mentalistpro <mentalistpro@users.noreply.github.com>
2020-02-18 15:54:50 +1100

    Add Wurt and Wortox quotes (non-DST)

--- modmain.lua
+++ modmain.lua
@@ -1,73 +1,79 @@
-Assets = 
-{
-    Asset("ATLAS", "images/inventoryimages/city_lamp.xml"),
-    Asset("IMAGE", "images/inventoryimages/city_lamp.tex"),
-    Asset("ATLAS", "minimap/city_lamp.xml"),
-    Asset("IMAGE", "minimap/city_lamp.tex"),
-}
-
+--List of prefabs and image assets in this mod. 
 PrefabFiles = 
 {
 	"city_lamp"
 }
 
+Assets = 
+{
+    Asset("ATLAS", "images/inventoryimages/city_lamp.xml")
+}
+
+AddMinimapAtlas("images/inventoryimages/city_lamp.xml")
+
+------------------------------------------------------------------------------------
+
+--//CONTENT//
+--1. Configs
+--2. Recipes
+--3. Strings
+
+------------------------------------------------------------------------------------
+--1. Configs
+
+if GetModConfigData("recipe") == 0 then
+	_Ingredient = Ingredient("goldnugget", 2) 
+elseif GetModConfigData("recipe") == 0 then
+	_Ingredient = Ingredient("goldnugget", 10)
+elseif GetModConfigData("recipe") == 0 then
+	_Ingredient = Ingredient("redgem", 1)
+elseif GetModConfigData("recipe") == 0 then
+	_Ingredient = Ingredient("alloy", 1)
+end
+
+TUNING.CITYLAMP_LIGHT_ALWAYS_ON = GetModConfigData("ison")
+TUNING.CITYLAMP_LIGHT_RADIUS = GetModConfigData("radius")
+TUNING.CITYLAMP_MINIMAP = GetModConfigData("icon")
+TUNING.CITYLAMP_SKIN= GetModConfigData("skin")
+
+AddPrefabPostInit("nightmarefuel", function(inst)
+	inst:AddComponent("tradable") -- so nightmare can be given to city_lamp
+end)
+
+------------------------------------------------------------------------------------
+--2. Recipes
+
 local _G = GLOBAL
-local _S = _G.STRINGS
 local Ingredient = _G.Ingredient
 local RECIPETABS = _G.RECIPETABS
 local TECH = _G.TECH
 
-------------------------------------------------------------------------
---Config
-
-local lp_ingredient = Ingredient("redgem", 1)
-if 	GetModConfigData("rec_difficulty") == 1 then	
-	lp_ingredient = Ingredient("goldnugget", 10)
-elseif GetModConfigData("rec_difficulty") == 2 then	
-	lp_ingredient = Ingredient("goldnugget", 2) 
-end
-
-local function TradableTag(inst) 
-	inst:AddComponent("tradable") 
-end
-
-AddPrefabPostInit("nightmarefuel",TradableTag)
-AddMinimapAtlas("minimap/city_lamp.xml")
-
-TUNING.CITYLAMP_LAMP_SKIN = GetModConfigData("lamp_skin")
-TUNING.CITYLAMP_LIGHT_RADIUS = GetModConfigData("light_radius")
-TUNING.CITYLAMP_LIGHT_ALWAYS_ON = GetModConfigData("light_always_on")
-TUNING.CITYLAMP_MINIMAP = GetModConfigData("minimap_icon")
-
-------------------------------------------------------------------------------------
---Recipe
-
-local city_lamp = AddRecipe("city_lamp",
+AddRecipe(
+	"city_lamp",
 	{
-	lp_ingredient,
-    Ingredient("transistor", 1),
-    Ingredient("lantern", 1)
+		_Ingredient,
+		Ingredient("transistor", 1),
+		Ingredient("lantern", 1)
 	},
 	RECIPETABS.LIGHT,
 	TECH.SCIENCE_TWO,
 	"city_lamp_placer",
-	nil,
-	nil,
-	nil,
-	nil,
+	nil, nil, nil, nil,
 	"images/inventoryimages/city_lamp.xml")
 
 ------------------------------------------------------------------------------------
---Strings
+--3. Strings
+
+local _S = _G.STRINGS
+
+if _S.CHARACTERS.WALANI == nil then 	_S.CHARACTERS.WALANI = { DESCRIBE = {},} end -- DLC002
+if _S.CHARACTERS.WARBUCKS == nil then 	_S.CHARACTERS.WARBUCKS = { DESCRIBE = {},} end -- DLC003
+if _S.CHARACTERS.WHEELER == nil then 	_S.CHARACTERS.WHEELER = { DESCRIBE = {},} end -- DLC003
+if _S.CHARACTERS.WILBA == nil then 		_S.CHARACTERS.WILBA = { DESCRIBE = {},} end -- DLC003
+if _S.CHARACTERS.WOODLEGS == nil then 	_S.CHARACTERS.WOODLEGS = { DESCRIBE = {},} end -- DLC002
 
 _S.NAMES.CITY_LAMP = "Lamp Post"
 _S.RECIPE_DESC.CITY_LAMP = "I can't believe I can make this."
-
-if _S.CHARACTERS.WALANI == nil then _S.CHARACTERS.WALANI = { DESCRIBE = {},} end -- DLC002
-if _S.CHARACTERS.WARBUCKS == nil then _S.CHARACTERS.WARBUCKS = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WHEELER == nil then _S.CHARACTERS.WHEELER = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WILBA == nil then _S.CHARACTERS.WILBA = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WOODLEGS == nil then _S.CHARACTERS.WOODLEGS = { DESCRIBE = {},} end -- DLC002
 
 _S.CHARACTERS.GENERIC.DESCRIBE.CITY_LAMP 		= { GENERIC = "Plain fire. No science involved.", ON = "Lusterless.",}
 _S.CHARACTERS.WALANI.DESCRIBE.CITY_LAMP 		= { GENERIC = "Thanks for keeping me safe, light!", ON = "Pretty sure that's a lamp.",}
@@ -86,3 +92,8 @@ _S.CHARACTERS.WOODIE.DESCRIBE.CITY_LAMP 		= { GENERIC = "A little bit of safe ha
 _S.CHARACTERS.WOODLEGS.DESCRIBE.CITY_LAMP 		= { GENERIC = "Sanctuary!", ON = "A landlamp.",}
 _S.CHARACTERS.WORMWOOD.DESCRIBE.CITY_LAMP 		= { GENERIC = "Big light stick", ON = "No light",}
 _S.CHARACTERS.WX78.DESCRIBE.CITY_LAMP 			= { GENERIC = "PRIMITIVE SOURCE OF ILLUMINATION", ON = "NONFUNCTIONING",}
+
+--Not original quotes
+_S.CHARACTERS.WORTOX.DESCRIBE.CITY_LAMP 		= { GENERIC = "It makes light out of nowhere!", ON = "Night is now hilarious, heehee!",}
+_S.CHARACTERS.WURT.DESCRIBE.CITY_LAMP 			= { GENERIC = "Big science glowy!", ON = "No more scary night lady, flort!",}
+
