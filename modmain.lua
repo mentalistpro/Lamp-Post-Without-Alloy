1d80d82d117e4a923658ba37d5725b46c943c69a
mentalistpro <mentalistpro@users.noreply.github.com>
2020-04-08 16:05:36 +1000

    TE-1.3

--- modmain.lua
+++ modmain.lua
@@ -1,98 +1,258 @@
-PrefabFiles = 
+PrefabFiles =
 {
-	"city_lamp"
 }
 
-Assets = 
+Assets =
 {
     Asset("ATLAS", "images/inventoryimages/city_lamp.xml")
 }
 
-------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
---//CONTENT//
---1. Configs
---2. Recipes
---3. Strings
+--[[[CONTENT]]
+--#1 Configs
+--#2 City_lamp
+--#3 PostInit
 
-------------------------------------------------------------------------------------
---1. Configs
+-----------------------------------------------------------------------------------------
+--#1 Configs
 
-if GetModConfigData("recipe") == 0 then
-	_Ingredient = Ingredient("goldnugget", 2) 
-elseif GetModConfigData("recipe") == 1 then
-	_Ingredient = Ingredient("goldnugget", 10)
-elseif GetModConfigData("recipe") == 2 then
-	_Ingredient = Ingredient("redgem", 1)
-elseif GetModConfigData("recipe") == 3 then
-	_Ingredient = Ingredient("alloy", 1)
-end
+AddMinimapAtlas("images/inventoryimages/city_lamp.xml")
 
 TUNING.CITYLAMP_LIGHT_ALWAYS_ON = GetModConfigData("ison")
 TUNING.CITYLAMP_LIGHT_RADIUS = GetModConfigData("radius")
 TUNING.CITYLAMP_MINIMAP = GetModConfigData("icon")
 TUNING.CITYLAMP_SKIN= GetModConfigData("skin")
 
-AddMinimapAtlas("images/inventoryimages/city_lamp.xml")
-
 AddPrefabPostInit("nightmarefuel", function(inst)
-	inst:AddComponent("tradable") -- so nightmare can be given to city_lamp
+    inst:AddComponent("tradable") -- so nightmare can be given to city_lamp
 end)
 
-------------------------------------------------------------------------------------
---2. Recipes
+-----------------------------------------------------------------------------------------
+--#2 City_lamp
 
 local _G = GLOBAL
-local Ingredient = _G.Ingredient
-local RECIPETABS = _G.RECIPETABS
-local TECH = _G.TECH
+local INTENSITY = 0.6
 
-AddRecipe(
-	"city_lamp",
-	{
-		_Ingredient,
-		Ingredient("transistor", 1),
-		Ingredient("lantern", 1)
-	},
-	RECIPETABS.LIGHT,
-	TECH.SCIENCE_TWO,
-	"city_lamp_placer",
-	nil, nil, nil, nil,
-	"images/inventoryimages/city_lamp.xml")
+local function fadein(inst)
+    inst.components.fader:StopAll()
+    inst.AnimState:PlayAnimation("on")
+    inst.AnimState:PushAnimation("idle", true)
+    inst.SoundEmitter:PlaySound("Hamlet/common/objects/city_lamp/fire_on")
+    inst.Light:Enable(true)
 
-------------------------------------------------------------------------------------
---3. Strings
+    if inst:IsAsleep() then
+        inst.Light:SetIntensity(INTENSITY)
+    else
+        inst.components.fader:Fade(0, INTENSITY, 3+math.random()*2, function(v) inst.Light:SetIntensity(v) end)
+        inst.Light:SetIntensity(0)
+    end
+end
 
-local _S = _G.STRINGS
+local function fadeout(inst)
+    inst.components.fader:StopAll()
+    inst.AnimState:PlayAnimation("off")    
+    inst.AnimState:PushAnimation("idle", true)
 
-if _S.CHARACTERS.WALANI == nil then 	_S.CHARACTERS.WALANI = { DESCRIBE = {},} end -- DLC002
-if _S.CHARACTERS.WARBUCKS == nil then 	_S.CHARACTERS.WARBUCKS = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WHEELER == nil then 	_S.CHARACTERS.WHEELER = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WILBA == nil then 		_S.CHARACTERS.WILBA = { DESCRIBE = {},} end -- DLC003
-if _S.CHARACTERS.WOODLEGS == nil then 	_S.CHARACTERS.WOODLEGS = { DESCRIBE = {},} end -- DLC002
+    if inst:IsAsleep() then
+        inst.Light:SetIntensity(0)
+    else
+        inst.components.fader:Fade(INTENSITY, 0, .75+math.random()*1, function(v) inst.Light:SetIntensity(v) end)
+    end
+end
 
-_S.NAMES.CITY_LAMP = "Lamp Post"
-_S.RECIPE_DESC.CITY_LAMP = "I can't believe I can make this."
+local function updatelight_new(inst)
+    if not _G.TheWorld.state.isday or TUNING.CITYLAMP_LIGHT_ALWAYS_ON == 1 then
+        if not inst.lighton then
+            inst:DoTaskInTime(math.random()*2, function() fadein(inst) end)
+        else
+            inst.Light:Enable(true)
+            inst.Light:SetIntensity(INTENSITY)
+        end
+        inst.AnimState:Show("FIRE")
+        inst.AnimState:Show("GLOW")
+        inst.lighton = true
 
-_S.CHARACTERS.GENERIC.DESCRIBE.CITY_LAMP 		= { GENERIC = "Plain fire. No science involved.", ON = "Lusterless.",}
-_S.CHARACTERS.WALANI.DESCRIBE.CITY_LAMP 		= { GENERIC = "Thanks for keeping me safe, light!", ON = "Pretty sure that's a lamp.",}
-_S.CHARACTERS.WARBUCKS.DESCRIBE.CITY_LAMP 		= { GENERIC = "What an intriguing civilization!", ON = "No need for light in the daytime, I suppose.",}
-_S.CHARACTERS.WARLY.DESCRIBE.CITY_LAMP 			= { GENERIC = "It's a small comfort.", ON = "What a quaint street lamp.",}
-_S.CHARACTERS.WATHGRITHR.DESCRIBE.CITY_LAMP		= { GENERIC = "It shines bright this night.", ON = "Nary a glimmer.",}
-_S.CHARACTERS.WAXWELL.DESCRIBE.CITY_LAMP 		= { GENERIC = "A welcome sign of civilization.", ON = "It eases my mind.",}
-_S.CHARACTERS.WEBBER.DESCRIBE.CITY_LAMP 		= { GENERIC = "They don't turn off when I get close!", ON = "I miss street lamps.",}
-_S.CHARACTERS.WENDY.DESCRIBE.CITY_LAMP 			= { GENERIC = "It cannot ward off the darkness forever.", ON = "A place to hold light.",}
-_S.CHARACTERS.WHEELER.DESCRIBE.CITY_LAMP 		= { GENERIC = "A free light source. I'll take it.", ON = "Not much to look at now, but wait'll nighttime.",}
-_S.CHARACTERS.WICKERBOTTOM.DESCRIBE.CITY_LAMP 	= { GENERIC = "Quite radiant.", ON = "A dormant street lamp.",}
-_S.CHARACTERS.WILBA.DESCRIBE.CITY_LAMP 			= { GENERIC = "'TIS A LAMP", ON = "'TIS ONLY ALIGHT AT NIGHT",}
-_S.CHARACTERS.WILLOW.DESCRIBE.CITY_LAMP 		= { GENERIC = "Fire is so versatile.", ON = "They're kinda dull in the daytime.",}
-_S.CHARACTERS.WOLFGANG.DESCRIBE.CITY_LAMP 		= { GENERIC = "Wolfgang does not like the dark.", ON = "Is lamp for night lights.",}
-_S.CHARACTERS.WOODIE.DESCRIBE.CITY_LAMP 		= { GENERIC = "A little bit of safe haven.", ON = "Looks like a street lamp.",}
-_S.CHARACTERS.WOODLEGS.DESCRIBE.CITY_LAMP 		= { GENERIC = "Sanctuary!", ON = "A landlamp.",}
-_S.CHARACTERS.WORMWOOD.DESCRIBE.CITY_LAMP 		= { GENERIC = "Big light stick", ON = "No light",}
-_S.CHARACTERS.WX78.DESCRIBE.CITY_LAMP 			= { GENERIC = "PRIMITIVE SOURCE OF ILLUMINATION", ON = "NONFUNCTIONING",}
+    elseif _G.TheWorld.state.isday and TUNING.CITYLAMP_LIGHT_ALWAYS_ON == 0 then
+        if inst.lighton then
+            inst:DoTaskInTime(math.random()*2, function() fadeout(inst) end)
+        else
+            inst.Light:Enable(false)
+            inst.Light:SetIntensity(0)
+        end
+        inst.AnimState:Hide("FIRE")
+        inst.AnimState:Hide("GLOW")
+        inst.lighton = false
+    end
+end
 
---Not original quotes
-_S.CHARACTERS.WORTOX.DESCRIBE.CITY_LAMP 		= { GENERIC = "It makes light out of nowhere!", ON = "Night is now hilarious, heehee!",}
-_S.CHARACTERS.WURT.DESCRIBE.CITY_LAMP 			= { GENERIC = "Big science glowy!", ON = "No more scary night lady, flort!",}
+local function updateaura(inst)
+    if inst.lightstate == "red" then
+        inst.Light:SetColour(.85,.35,.5)
+        inst.Light:SetIntensity(0.8)
+        inst.components.heater.heat = 30
+        inst.components.heater:SetThermics(true, false)
+    elseif inst.lightstate == "blue" then
+        inst.Light:SetColour(.4,1,1)
+        inst.Light:SetIntensity(0.8)
+        inst.components.heater.heat = -30
+        inst.components.heater:SetThermics(false, true)
+    else
+        inst.components.heater.heat = 0
+    end
 
+    --//TODO: aura functions have not been defined yet
+    if inst.lightstate == "orange" then
+        inst.Light:SetColour(1,.65,0)
+        --//It's about laziness
+        --AuraNoHungerCost
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = TUNING.SANITYAURA_SMALL_TINY
+
+    elseif inst.lightstate == "green" then
+        inst.Light:SetColour(.5,1,0)
+        --//It's about perfection
+        --AuraNoHalthCost
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = TUNING.SANITYAURA_SMALL_TINY
+
+    elseif inst.lightstate == "yellow" then
+        inst.Light:SetColour(1,1,0)
+        --//It's about heaven
+        --AuraHeal
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = TUNING.SANITYAURA_SMALL_TINY
+
+    elseif inst.lightstate == "purple" then
+        inst.Light:SetColour(.9,.2,.8)
+        --//It's about chaos
+        --AuraAttackSummonsThunder
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = -TUNING.SANITYAURA_TINY
+    --//TODO: END
+    elseif inst.lightstate == "black" then
+        inst.Light:SetColour(.5,.5,.5)
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = 0
+
+    elseif inst.lightstate == "white" then
+        inst.Light:SetColour(1,1,1)
+        inst.Light:SetIntensity(0.8)
+        inst.components.sanityaura.aura = 0
+
+    else
+        inst.Light:SetIntensity(0.6)
+        inst.components.sanityaura.aura = 0
+    end
+
+    if inst.lightstate == "default" then
+        inst.Light:SetColour(197/255, 197/255, 10/255)
+    end
+
+    --when light is OFF, effects must be OFF
+    if _G.TheWorld.state.isday and TUNING.CITYLAMP_LIGHT_ALWAYS_ON == 0 then
+        inst.components.heater.heat = 0
+        inst.components.sanityaura.aura = 0
+    end
+end
+
+local function ShouldAcceptItem(inst, item)
+    return inst.lighton and
+    (
+        item.prefab == "red_cap"        or item.prefab == "thulecite_pieces" or
+        item.prefab == "honey"          or item.prefab == "green_cap"        or
+        item.prefab == "blue_cap"       or item.prefab == "spidergland"      or
+        item.prefab == "nightmarefuel"  or item.prefab == "lightbulb"        or
+        item.prefab == "goldnugget"
+    )
+end
+
+local function OnGetItemFromPlayer(inst, giver, item)
+    if item.prefab == "red_cap" then
+        inst.lightstate = "red"
+    elseif item.prefab == "thulecite_pieces" then
+        inst.lightstate = "orange"
+    elseif item.prefab == "honey" then
+        inst.lightstate = "yellow"
+    elseif item.prefab == "green_cap" then
+        inst.lightstate = "green"
+    elseif item.prefab == "blue_cap" then
+        inst.lightstate = "blue"
+    elseif item.prefab == "spidergland" then
+        inst.lightstate = "purple"
+    elseif item.prefab == "nightmarefuel" then
+        inst.lightstate = "black"
+    elseif item.prefab == "lightbulb" then
+        inst.lightstate = "white"
+    elseif item.prefab == "goldnugget" then
+        inst.lightstate = "default"
+    end
+    updateaura(inst)
+    inst.SoundEmitter:PlaySound("dontstarve/common/fireAddFuel")
+end
+
+local function OnRefuseItem(inst, item)
+    inst.SoundEmitter:PlaySound("dontstarve/common/together/spot_light/electricity")
+    inst.AnimState:PlayAnimation("idle", true)
+end
+
+local function onsave_new (inst, data)
+    if inst.lighton then
+        data.lighton = inst.lighton
+    end
+
+    if inst.lightstate then
+        data.lightstate = inst.lightstate
+    end
+end
+
+local function onload_new(inst, data)
+    if data then
+        if data.lighton then
+            fadein(inst)
+            inst.Light:Enable(true)
+            inst.Light:SetIntensity(INTENSITY)
+            inst.AnimState:Show("FIRE")
+            inst.AnimState:Show("GLOW")
+            inst.lighton = true
+        end
+
+        inst.lightstate = data.lightstate
+        updateaura(inst)
+    end
+end
+
+-----------------------------------------------------------------------------------------
+--#3 PostInit
+
+AddPrefabPostInit("city_lamp", function(inst)
+    local minimap = inst.entity:AddMiniMapEntity()
+    if TUNING.CITYLAMP_MINIMAP == 0 then
+        minimap:SetIcon( "city_lamp.tex" )
+    end
+
+    inst.Light:SetRadius(TUNING.CITYLAMP_LIGHT_RADIUS)
+
+    if TUNING.CITYLAMP_SKIN == 0 then
+        inst.AnimState:SetBuild("lamp_post2_city_build")
+    elseif TUNING.CITYLAMP_SKIN == 1 then
+        inst.AnimState:SetBuild("lamp_post2_yotp_build")
+    end
+
+    inst:WatchWorldState("iscavedusk", function() updatelight_new(inst) end)
+    inst:WatchWorldState("iscaveday", function() updatelight_new(inst) end)
+    inst:WatchWorldState("isdusk", function() updateaura(inst) end)
+    inst:WatchWorldState("isday", function() updateaura(inst) end)
+
+    inst:AddComponent("trader")
+    inst.components.trader:SetAcceptTest(ShouldAcceptItem)
+    inst.components.trader.onaccept = OnGetItemFromPlayer
+    inst.components.trader.onrefuse = OnRefuseItem
+
+    inst:AddComponent("heater")
+    inst:AddComponent("lootdropper")
+    inst:AddComponent("sanityaura")
+
+    inst.onsave_new = onsave_new
+    inst.onload_new = onload_new
+end)
